4f158c68f1d9152d09fb60b508b432d0167457b3
waruna <waruna@duosoftware.com>
2017-07-18 14:00:25 +0530

    add sentinels setting

--- config/default.js
+++ config/default.js
@@ -1,9 +1,15 @@
 module.exports = {
     "Security": {
-        "ip": "45.55.142.207",
+        "ip" : "45.55.142.207",
         "port": 6389,
         "user": "duo",
-        "password": "DuoS123"
+        "password": "DuoS123",
+        "mode":"sentinel",//instance, cluster, sentinel
+        "sentinels":{
+            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "port":16389,
+            "name":"redis-cluster"
+        }
     },
     "Host": {
         "Ip": "0.0.0.0",
