996b2d9724ee7fc61ce5172e0cf266f4bc54131c
achinthau <achintha@duosoftware.com>
2017-07-24 12:01:39 +0530

    Update custom-environment-variables.js

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -4,16 +4,29 @@
 
 module.exports = {
 
-    "Host":{
-        "Port":"HOST_PHONENUMBERTRUNKSERVICE_PORT",
-        "Version":"HOST_VERSION"
+    "Host":
+    {
+        "version": "HOST_VERSION"
+        "vdomain": "LB_FRONTEND",
+        "ip": "HOST_NAME",
+        "port": "HOST_INTEGRATIONAPI_PORT",
+        "version": "HOST_VERSION",
+        "token": "HOST_TOKEN"
+      
     },
     "Security":
     {
+
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "mode":"SYS_REDIS_MODE",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
 
     },
     "Mongo":
@@ -23,5 +36,11 @@ module.exports = {
         "dbname":"SYS_MONGO_DB",
         "password":"SYS_MONGO_PASSWORD",
         "user":"SYS_MONGO_USER"
-    }
+    },
+    
+   "Services": {
+        "userserviceurl": "SYS_USERSERVICE_HOST",
+        "userserviceport": 'SYS_USERSERVICE_PORT',
+        "userserviceversion": "SYS_USERSERVICE_VERSION"
+ }
 };
