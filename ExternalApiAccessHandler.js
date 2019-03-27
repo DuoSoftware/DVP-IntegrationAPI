7f1d7c19b72bf89bd4ea914a453681755544f040
sukitha <sukithaj@gmail.com>
2019-03-27 15:37:42 +0530

    added request module for call external

--- ExternalApiAccessHandler.js
+++ ExternalApiAccessHandler.js
@@ -10,6 +10,8 @@ var async = require('async');
 var externalProfileHandler = require('./ExternalProfileHandler.js');
 var url_util = require('url');
 var ESB = require('./light-esb-node.js');
+var format = require("string-template");
+const request = require('request');
 
 var traverseObject = function (obj, param, isInner) {
     var val = null;
@@ -195,7 +197,7 @@ var generateAPICalls = function (reqId, apiDetails, inputObject) {
     });
 };
 
-var callAppIntegration = function (integrationData, reqObj) {
+var callAppIntegrationx = function (integrationData, reqObj) {
     return new Promise(function (fulfill, reject) {
 
         let esbCallback = function(error,esbMessage){
@@ -215,7 +217,7 @@ var callAppIntegration = function (integrationData, reqObj) {
                     }
 
                     responseObject.ApiResponse = esbMessage.payload;
-                    
+
                     // check if the response status code is in list of accepted codes..
                     if(integrationData.response_map){
                         if(integrationData.response_map.accepted_codes.includes(esbMessage.status.code.toString())){
@@ -239,10 +241,10 @@ var callAppIntegration = function (integrationData, reqObj) {
                             responseObject.Success = false;
                         }
                     }
-                    
+
                     fulfill(responseObject);
                     return;
-                } 
+                }
             }catch(e){
                 reject(e);
                 return;
@@ -257,19 +259,19 @@ var callAppIntegration = function (integrationData, reqObj) {
 
         if(integrationData.parameters) {
             var paramError = false;
-        
+
             for(param of integrationData.parameters){
                 let paramVal;
-                    
+
                 // if the parameter referenceObject is Custom, then we don't refer to any object, just use the property as value.
                 if(param.referenceObject == "Custom") {
                     paramVal = param.referenceProperty;
                 }else{
                     paramVal = reqObj[param.referenceObject][param.referenceProperty];
                 }
-              
+
                 if(paramVal != "" && !paramVal){
-                    // paramter cannot be null , undefined, throw error! (but could be empty! as requested by the developer)                    
+                    // paramter cannot be null , undefined, throw error! (but could be empty! as requested by the developer)
                     paramError = true;
                     break;
                 }else{
@@ -306,16 +308,201 @@ var callAppIntegration = function (integrationData, reqObj) {
             esbCallback(new Error('Missing parameter value for ' + param.referenceObject + '.' + param.referenceProperty));
         }else{
             let ESBMessage = ESB.createMessage(paramObj.BODY);
-            
+
             let loggerComponent = ESB.createLoggerComponent(esbCallback);
             let requestComponent = ESB.createCallComponent(esbCallback, integrationData.url, integrationData.method, paramObj.PARAMS, paramObj.QUERY, authProperties, requestProperties);
-            let resultComponent = ESB.createResultComponent(esbCallback);  
-            
+            let resultComponent = ESB.createResultComponent(esbCallback);
+
             requestComponent.connect(loggerComponent);
             requestComponent.connect(resultComponent);
             requestComponent.send(ESBMessage);
+
         }
     });
+
+};
+
+
+var callAppIntegration = async function (integrationData, reqObj) {
+    //return new Promise(function (fulfill, reject) {
+
+    let requestMethod = async function (option) {
+
+        return new Promise(
+            function (resolve, reject) {
+
+                request(option, function (error, response, body) {
+                    if(error)
+                        return reject(error);
+                    return resolve(response);
+                });
+
+            }
+        );
+
+    }
+
+    let options = {
+        method: integrationData.method.toUpperCase(),
+        uri: integrationData.url
+    };
+
+    let paramObj = {
+        "PARAMS": {},
+        "QUERY": {},
+        "BODY": {}
+    };
+
+    if (integrationData.parameters) {
+        var paramError = false;
+
+        for (let param of integrationData.parameters) {
+            let paramVal;
+
+            // if the parameter referenceObject is Custom, then we don't refer to any object, just use the property as value.
+            if (param.referenceObject == "Custom") {
+                paramVal = param.referenceProperty;
+            } else {
+                paramVal = reqObj[param.referenceObject][param.referenceProperty];
+            }
+
+            if (paramVal != "" && !paramVal) {
+                // paramter cannot be null , undefined, throw error! (but could be empty! as requested by the developer)
+                paramError = true;
+                break;
+            } else {
+                paramObj[param.parameterLocation][param.name] = paramVal;
+            }
+        };
+    };
+
+
+    if (integrationData.auth_type == "Basic") {
+        options.auth = {};
+        options.auth.user = integrationData.username;
+        options.auth.pass = integrationData.password;
+    } else if (integrationData.auth_type == "Bearer") {
+        options.auth = {
+            bearer: integrationData.token
+        }
+    };
+
+    let requestProperties = {
+        timeout: 10000, //milliseconds,
+        trycount: 5
+    };
+
+    if (integrationData.app && integrationData.app.timeout) {
+        requestProperties.timeout = parseInt(integrationData.app.timeout) * 1000;
+    }
+
+    if (integrationData.app && integrationData.app.trycount) {
+        requestProperties.trycount = parseInt(integrationData.app.trycount);
+    }
+
+    options.timeout = requestProperties.timeout;
+
+    if (paramObj.QUERY) {
+        options.qs = paramObj.QUERY;
+    }
+
+    if (paramObj.BODY && options.method != "GET") {
+        options.json = paramObj.BODY
+    }else{
+        options.json = true;
+    }
+
+
+    if (paramObj.PARAMS) {
+        options.uri = format(options.uri, paramObj.PARAMS);
+    }
+
+
+    for (let i = 0; i <= requestProperties.trycount; i++) {
+        try {
+
+
+            let result = await requestMethod(options);
+
+            // if(result){
+            //     if(integrationData.response_map){
+            //         if(integrationData.response_map.accepted_codes.includes(esbMessage.status.code.toString())){
+            //             responseObject.Success = true;
+            //             responseObject.Message = integrationData.response_map.success_msg;
+            //         }else{
+            //             responseObject.Success = false;
+            //             responseObject.Message = integrationData.response_map.error_msg;
+            //         }
+            //
+            //         responseObject.Message = responseObject.Message.replace(/{(.*)}/, function(match, placeholder){
+            //             return placeholder.trim().split('.').reduce((o,i)=>o[i], responseObject.ApiResponse);
+            //         });
+            //     }else{
+            //         // response map is not defined (a default integration?), so we get the response status from the EsbMessage
+            //         responseObject.Message = esbMessage.status.message;
+            //
+            //         if(parseInt(esbMessage.status.code) >= 200 && parseInt(esbMessage.status.code) < 300){ //a 2** response
+            //             responseObject.Success = true;
+            //         }else{
+            //             responseObject.Success = false;
+            //         }
+            //     }
+            // }
+            if (result) {
+
+
+                let responseObject = {};
+                responseObject.ApiResponse = result.body;
+                if (integrationData.response_map) {
+                    if (integrationData.response_map.accepted_codes.includes(result.statusCode.toString())) {
+                        responseObject.Success = true;
+                        responseObject.Message = integrationData.response_map.success_msg;
+                    } else {
+                        responseObject.Success = false;
+                        responseObject.Message = integrationData.response_map.error_msg;
+                    }
+
+                    responseObject.Message = responseObject.Message.replace(/{(.*)}/, function (match, placeholder) {
+                        return placeholder.trim().split('.')
+                            .reduce((o, i) => o[i], responseObject.ApiResponse);
+                    });
+                } else {
+                    // response map is not defined (a default integration?), so we get the response status from the EsbMessage
+                    responseObject.Message = result.body;
+
+                    if (parseInt(result.statusCode) >= 200 && parseInt(result.statusCode) < 300) { //a 2** response
+                        responseObject.Success = true;
+                    } else {
+                        responseObject.Success = false;
+                    }
+                }
+                return responseObject;
+
+            }
+
+        } catch (ex) {
+
+            console.log(ex.message);
+        }
+    }
+
+
+    let responseObject = {};
+    if (integrationData.response_map) {
+
+        responseObject.Success = false;
+        responseObject.Message = integrationData.response_map.error_msg;
+    } else {
+        // response map is not defined (a default integration?), so we get the response status from the EsbMessage
+
+
+        responseObject.Success = false;
+
+    }
+    return responseObject;
+
+
+    //});
 
 };
 
