012441d5b480df8d7e9539fe1a85161467e7ca69
DuoB Coder <duobcoder@gmail.com>
2020-05-03 16:57:49 +0530

    Update Packages

--- config/default.js
+++ config/default.js
@@ -1,12 +1,12 @@
 module.exports = {
     "Security": {
-        "ip" : "45.55.142.207",
+        "ip" : "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
-        "mode":"sentinel",//instance, cluster, sentinel
+        "user": "",
+        "password": "",
+        "mode":"instance",//instance, cluster, sentinel
         "sentinels":{
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port":16389,
             "name":"redis-cluster"
         }
@@ -18,11 +18,12 @@ module.exports = {
         "token": ""
     },
     "Mongo": {
-        "ip":"104.236.231.11",
+        "ip":"",
         "port":"27017",
-        "dbname":"dvpdb",
-        "password":"DuoS123",
-        "user":"duo"
+        "dbname":"",
+        "password":"",
+        "user":"",
+        "type": "mongodb+srv"
     },
     "Services": {
         "userserviceurl": "SYS_USERSERVICE_HOST",
